9b9f645a98403a3268e0488d649d11edabf3c138
Kayla Hammond Larkin <larkink@BUMC-dot1x-155-41-182-170.bumc.bu.edu>
2018-11-15 12:50:25 -0500

    Updated again whoops

--- sandbox/meshminer/meshminer5.py
+++ sandbox/meshminer/meshminer5.py
@@ -1,63 +1,47 @@
 # Import libraries
-import os
-from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
-from werkzeug.utils import secure_filename
 import lxml.html
 import requests
 import re
 import pandas as pd
 
+# Create empty df for MeSH terms data
+mesh_tally_cols = ['raw_term', 'Frequency', 'URL', 'Term']
+mesh_tally = pd.DataFrame(columns = mesh_tally_cols)
 
-# Import libraries
-import operator
-
-# Create empty dictionary for MeSH terms
-mesh_tally = {}
-
-
-# Create PMID list
-
-# FROM TXT FILE
-#with open('hipac.txt', 'r') as input_file: 
-#    pmid_str=input_file.read().replace('\n', ' ')
-
-# FROM STR
 pmid_str = "30170574, 30130317, 30115126, 30089083, 30088578, 30046807, 29999103, 29966882, 29927272, 29902142"
 
 pmid_list = re.findall(r'\d+', pmid_str)
-print("PMID List: ", pmid_list)
 
 for pmid in pmid_list:
     # Assemble URLs for articles
     url = 'https://www.ncbi.nlm.nih.gov/pubmed/' + str(pmid)
-    print(url)
-    
+
     # Make request
     r = requests.get(url)
     root = lxml.html.fromstring(r.content)
-    
+
     # Find MeSH terms
     mesh_terms = root.xpath('//li/a[@alsec="mesh"]/text()')
-    for term in mesh_terms: 
+    for term in mesh_terms:
         if '/' in term:
             term = term.rsplit('/')[0]
         if '*' in term:
             term = term.rsplit('*')[0]
-        if term in mesh_tally.keys():
-            mesh_tally[term] += 1
-        else:
-            mesh_tally[term] = 1
-
-# Sort mesh_tally dictionary by value in ascending order
-sorted_mesh_tally = sorted(mesh_tally.items(), key=lambda x: (x[1],x[0]), reverse=True)
-
-print(sorted_mesh_tally)
-
-# Create output dataframe
-output_columns = ['MeSH Term', 'Frequency']
-output_df = pd.DataFrame(data=sorted_mesh_tally, columns=output_columns)
-print(output_df.head())
-
-# Create output CSV
-output_df.to_csv("output.csv", index=False)
-
+        if (mesh_tally.raw_term == term).any():
+            mesh_tally.loc[mesh_tally.raw_term == term, 'Frequency'] += 1
+        if not (mesh_tally.raw_term == term).any():
+            mesh_tally_dict = {}
+            mesh_tally_dict['raw_term'] = term
+            mesh_tally_dict['Frequency'] = 1
+            if len(term.split()) > 1:
+                plussed_term = term.replace(" ", "+")
+                mesh_tally_dict['URL'] = 'https://www.ncbi.nlm.nih.gov/mesh?term=' + plussed_term
+            else:
+                mesh_tally_dict['URL'] =  'https://www.ncbi.nlm.nih.gov/mesh?term=' + term  
+            mesh_tally_dict['Term'] = "<a href='" + mesh_tally_dict["URL"] + "'>" + mesh_tally_dict['raw_term'] + "</a>"
+            mesh_tally = mesh_tally.append(mesh_tally_dict, ignore_index=True)
+            mesh_tally2 = mesh_tally[['Term','Frequency']].copy()
+    
+mesh_tally2.to_csv('mesh_tally2.csv', index=False)
+        
+print(mesh_tally2)
