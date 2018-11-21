9fa8d12038cd33e6fd9d204b2bcb340a7f1c6901
Kayla Hammond Larkin <larkink@BUMC-dot1x-155-41-160-31.bumc.bu.edu>
2018-11-21 16:59:25 -0500

    Update

--- sandbox/meshpie/meshpie1.py
+++ sandbox/meshpie/meshpie1.py
@@ -3,7 +3,13 @@ import lxml.html
 import requests
 import re
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import table
+import seaborn as sns
+import numpy as np
+import io
+import base64
+import urllib.parse
 
 # Create empty df for MeSH terms data
 mesh_tally_cols = ['Term','Frequency', 'URL']
@@ -46,13 +52,54 @@ mesh_tally.to_csv('mesh_tally.csv', index=False)
         
 print(mesh_tally)
 
-#table = BeautifulSoup(mesh_tally.to_html(index=False),  'html5lib')
-#table.find('table')['id'] = 'results'
-#table.find('table')['width'] = '100%'
-
 with pd.option_context('display.max_colwidth', -1): 
     table = mesh_tally.to_html(index=False).replace('<table','<table class="table-striped table-bordered dt-responsive nowrap" id="results" style="width:100%;"')
 
 with open("output1.html", "w") as file:
     file.write(str(table))
     
+# Create pie chart with top MeSH terms by frequency
+
+mesh_tally['Frequency'].plot(kind='pie', subplots=True) 
+plt.show()
+
+mesh_tally['Frequency'].plot(kind='pie', labels=mesh_tally['Term'], autopct='%.2f', fontsize=20)
+plt.show()
+
+top_mesh_tally =  mesh_tally.nlargest(10, 'Frequency')
+
+top_mesh_tally['Frequency'].plot(
+        kind='pie', 
+        labels=top_mesh_tally['Term'], 
+        fontsize=10, 
+        title= "Top Ten MeSH Terms")
+plt.show()
+
+
+# Create bar chart
+
+vert_fig = sns.barplot(top_mesh_tally['Term'], top_mesh_tally['Frequency'], alpha=0.8,  palette="Blues_d")
+plt.title('Top Ten MeSH Terms')
+plt.ylabel('Frequency', fontsize=12)
+plt.xlabel('MeSH Term', fontsize=12)
+plt.xticks(rotation=90)
+plt.show(vert_fig)
+
+horiz_fig = sns.barplot(top_mesh_tally['Frequency'], top_mesh_tally['Term'], palette="pastel")
+plt.title('Top Ten MeSH Terms')
+plt.ylabel('MeSH Term', fontsize=12)
+plt.xlabel('Frequency', fontsize=12)
+plt.show(horiz_fig)
+
+
+img = io.BytesIO()  # create the buffer
+sns.barplot(top_mesh_tally['Frequency'], top_mesh_tally['Term'], palette="pastel")
+plt.title('Top Ten MeSH Terms')
+plt.ylabel('MeSH Term', fontsize=12)
+plt.xlabel('Frequency', fontsize=12)
+
+plt.savefig(img, format='png')  # save figure to the buffer
+img.seek(0)  # rewind your buffer
+plot_data = urllib.parse.quote(base64.b64encode(img.read()).decode()) # base64 encode & URL-escape
+#return render_template('plot.html', plot_url=plot_data)
+
