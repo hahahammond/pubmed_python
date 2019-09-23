85bd8aed3bd1b3d3823fd8135c88b54a500f8b13
Kayla Hammond Larkin <larkink@BUMC-dot1x-155-41-164-27.bumc.bu.edu>
2019-09-23 14:00:32 -0400

    Added manual meshminer file

---
+++ manual_meshminer_09.23.2019.py
@@ -0,0 +1,64 @@
+# Import libraries
+import lxml.html
+import requests
+import re
+import pandas as pd
+
+
+# Create empty df for MeSH terms data
+mesh_tally_cols = ['Term','Frequency', 'URL']
+mesh_tally = pd.DataFrame(columns = mesh_tally_cols)
+
+#####
+
+#pmid_str = "30170574, 30130317, 30115126, 30089083, 30088578, 30046807, 29999103, 29966882, 29927272, 29902142"
+
+with open('pmid_file.txt', 'r') as input_file: 
+    pmid_str=input_file.read()
+    pmid_list = re.findall(r'\d+', pmid_str)
+
+#####
+
+for pmid in pmid_list:
+    # Assemble URLs for articles
+    url = 'https://www.ncbi.nlm.nih.gov/pubmed/' + str(pmid)
+
+    # Make request
+    r = requests.get(url)
+    root = lxml.html.fromstring(r.content)
+
+    # Find MeSH terms
+    mesh_terms = root.xpath('//li/a[@alsec="mesh"]/text()')
+    for term in mesh_terms:
+        if '/' in term:
+            term = term.rsplit('/')[0]
+        if '*' in term:
+            term = term.rsplit('*')[0]
+        if (mesh_tally.Term == term).any():
+            mesh_tally.loc[mesh_tally.Term == term, 'Frequency'] += 1
+        if not (mesh_tally.Term == term).any():
+            mesh_tally_dict = {}
+            mesh_tally_dict['Term'] = term
+            mesh_tally_dict['Frequency'] = 1
+            if len(term.split()) > 1:
+                plussed_term = term.replace(" ", "+")
+                mesh_tally_dict['URL'] = 'https://www.ncbi.nlm.nih.gov/mesh?term=' + plussed_term
+            else:
+                mesh_tally_dict['URL'] =  'https://www.ncbi.nlm.nih.gov/mesh?term=' + term  
+            mesh_tally = mesh_tally.append(mesh_tally_dict, ignore_index=True)
+            mesh_tally['Frequency'] = mesh_tally['Frequency'].astype(int)
+    
+mesh_tally.to_csv('mesh_tally.csv', index=False)
+        
+print(mesh_tally)
+
+#table = BeautifulSoup(mesh_tally.to_html(index=False),  'html5lib')
+#table.find('table')['id'] = 'results'
+#table.find('table')['width'] = '100%'
+
+with pd.option_context('display.max_colwidth', -1): 
+    table = mesh_tally.to_html(index=False).replace('<table','<table class="table-striped table-bordered dt-responsive nowrap" id="results" style="width:100%;"')
+
+with open("output1.html", "w") as file:
+    file.write(str(table))
+    
